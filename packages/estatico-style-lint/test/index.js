b83affebc8dcc980f72e9b08163cd724b019eb63
Thomas Jaggi <thomas.jaggi@unic.com>
2018-01-24 22:57:37 +0100

    [Chore] estatico-style-lint: Fixed tests

--- packages/estatico-style-lint/test/index.js
+++ packages/estatico-style-lint/test/index.js
@@ -13,4 +13,18 @@ const defaults = {
   dest: './test/results/',
 };
 
+const stripLog = str => stripAnsi(str.replace(/\n/gm, '').replace(/\t/g, ' ')).replace(/\s\s+/g, ' ');
+
+test.cb('default', (t) => {
+  const spy = sinon.spy(console, 'log');
+
+  task(defaults).on('end', () => {
+    spy.restore();
+
+    console.log(stripLog(spy.getCall(0).args.join(' ')));
+
+    t.end();
+  });
+});
+
 test.afterEach(() => del(path.join(__dirname, '/results')));
