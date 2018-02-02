faad097b7fb06b4d5a91ae54cedb61c2681978cb
Thomas Jaggi <thomas.jaggi@unic.com>
2018-02-02 19:21:11 +0100

    estatico-webpack: Updated to Webpack 4

--- packages/estatico-webpack/index.js
+++ packages/estatico-webpack/index.js
@@ -1,130 +1,129 @@
 const log = require('fancy-log');
 const chalk = require('chalk');
 const merge = require('lodash.merge');
+const webpack = require('webpack');
+const UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-const defaults = {
-  entries: null,
-  dest: null,
-  destBase: null,
-  plugins: {
-    uglify: {
+const defaults = dev => ({
+  webpack: {
+    mode: dev ? 'development' : 'production',
+    entry: null,
+    resolve: {
+      alias: {
+        handlebars: 'handlebars/runtime.js',
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /jquery\.js$/,
+          loader: 'expose-loader?$!expose-loader?jQuery',
+        },
+        {
+          test: /modernizrrc\.js$/,
+          loader: 'expose-loader?Modernizr!webpack-modernizr-loader',
+        },
+        {
+          test: /\.hbs$/,
+          loader: 'handlebars-loader',
+        },
+        {
+          test: /(\.js|\.jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                useBuiltIns: 'usage',
+                targets: {
+                  browsers: ['last 2 versions'],
+                },
+                // Disabled due to https://gist.github.com/jasonphillips/57c1f8f9dbcd8b489dafcafde4fcdba6
+                // loose: true,
+              }],
+            ],
+            plugins: [],
+          },
+        },
+      ],
+    },
+
+    // Minifiy in prod mode
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        // Path to bundle report file that will be generated in `static` mode.
+        // Relative to bundles output directory.
+        reportFilename: 'report.html',
+        openAnalyzer: false,
+      }),
+    ].concat(dev ? [] : [
+      new UnminifiedWebpackPlugin(),
+    ]),
+    output: {
+      path: null,
+      filename: `[name]${dev ? '' : '.min'}.js`,
+
+      // Save async loaded files (using require.ensurce) in special dir
+      chunkFilename: `assets/[name]${dev ? '' : '.min'}.js`,
+
+      // Tell webpack about the asset path structure in the browser to be able to load async files
+      // publicPath: path.join('/', path.relative(config.destBase, config.dest), '/'),
     },
   },
   errorHandler: (err) => {
-    log(`estatico-webpack${err.plugin ? ` (${err.plugin})` : null}`, chalk.cyan(err.fileName), chalk.red(err.message));
-  },
-  dev: false,
-};
+    log('estatico-webpack', chalk.red(err.message));
 
-module.exports = (options) => {
+    if (!dev) {
+      process.exit(1);
+    }
+  },
+});
+
+module.exports = (options, dev) => {
   let config = {};
 
   if (typeof options === 'function') {
-    config = options(defaults);
+    config = options(defaults(dev));
   } else {
-    config = merge({}, defaults, options);
-  }
-
-  // Validate options
-  if (!config.entries) {
-    throw new Error('\'options.entries\' is missing');
-  }
-  if (!config.dest) {
-    throw new Error('\'options.dest\' is missing');
+    config = merge({}, defaults(dev), options);
   }
 
   return (cb) => {
-    const webpack = require('webpack'); // eslint-disable-line global-require
-    const UnminifiedWebpackPlugin = require('unminified-webpack-plugin'); // eslint-disable-line global-require
-    const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer'); // eslint-disable-line global-require
     const once = require('lodash.once'); // eslint-disable-line global-require
     const logStats = require('./lib/log'); // eslint-disable-line global-require
 
-    const compiler = webpack({
-      entry: config.entries,
-      resolve: {
-        alias: {
-          handlebars: 'handlebars/runtime.js',
-        },
-      },
-      module: {
-        loaders: [
-          {
-            test: /jquery\.js$/,
-            loader: 'expose-loader?$!expose?jQuery',
-          },
-          {
-            test: /(\.js|\.jsx)$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                ['@babel/preset-env', {
-                  useBuiltIns: 'usage',
-                  targets: {
-                    browsers: ['last 2 versions'],
-                  },
-                  loose: true,
-                }],
-              ],
-              plugins: [],
-            },
-          },
-        ],
-      },
+    try {
+      const compiler = webpack(config.webpack);
 
-      // Minifiy in prod mode
-      plugins: [
-        new BundleAnalyzerPlugin({
-          analyzerMode: 'static',
-          // Path to bundle report file that will be generated in `static` mode.
-          // Relative to bundles output directory.
-          reportFilename: 'report.html',
-          openAnalyzer: false,
-        }),
-        new webpack.DefinePlugin({
-          'process.env': {
-            NODE_ENV: JSON.stringify('production'),
-          },
-        }),
-        new webpack.optimize.UglifyJsPlugin(config.plugins.uglify),
-        new UnminifiedWebpackPlugin(),
-      ],
-      output: {
-        path: config.dest,
-        filename: '[name].min.js',
+      const callback = (err, stats) => {
+        let done = cb;
 
-        // Save async loaded files (using require.ensurce) in special dir
-        chunkFilename: `${config.destAsyncSuffix}[name].min.js`,
+        if (config.watch) {
+          done = once(done);
+        }
 
-        // Tell webpack about the asset path structure in the browser to be able to load async files
-        // publicPath: path.join('/', path.relative(config.destBase, config.dest), '/'),
-      },
-      devtool: config.dev ? 'eval-cheap-module-source-map' : false,
-    });
+        if (err) {
+          config.errorHandler(err);
+        }
 
-    const callback = (err, stats) => {
-      let done = cb;
+        logStats(stats, 'estatico-webpack');
+
+        done();
+      };
 
       if (config.watch) {
-        done = once(done);
+        compiler.watch({}, callback);
+      } else {
+        compiler.run(callback);
       }
 
-      if (err) {
-        config.errorHandler(err);
-      }
+      return compiler;
+    } catch (err) {
+      config.errorHandler(err);
 
-      logStats(stats, 'estatico-webpack');
-
-      done();
-    };
-
-    if (config.watch) {
-      compiler.watch({}, callback);
-    } else {
-      compiler.run(callback);
+      return cb();
     }
-
-    return compiler;
   };
 };
