f0fc1e1fd761dc1df7b02113d1e4422d5d60cef6
Thomas Jaggi <thomas.jaggi@unic.com>
2018-02-09 09:06:36 +0100

    estatico-utils: Logger: Log error stack in debug mode

--- packages/estatico-utils/lib/logger.js
+++ packages/estatico-utils/lib/logger.js
@@ -4,55 +4,63 @@ function Logger(pluginName) {
   const chalk = require('chalk');
   const log = require('fancy-log');
 
-  const debug = util.debuglog(pluginName);
-  const debugExtended = util.debuglog(`${pluginName}-extended`);
+  const debugLogger = util.debuglog(pluginName);
+  const debugLoggerExtended = util.debuglog(`${pluginName}-extended`);
+
+  /**
+   * Simple info logger
+   * @param {*} msg - Log content
+   */
+  const info = (msg) => {
+    log(chalk.blue(pluginName), msg);
+  };
+
+  /**
+   * Debug logger
+   * @param {string} step - Title of current task step, will be highlighted
+   * @param {*} msg - Log content
+   * @param {*} [extendedMsg] - Extended log content, will only be logged
+   * if `NODE_DEBUG=${pluginName}-extended` is set.
+   * The current Node version (9.5.0 at the time of writing) seems to support wildcards
+   * for sections, so `NODE_DEBUG=${pluginName}*` should be fine at some point, too.
+   */
+  const debug = (step, msg, extendedMsg) => {
+    // Highlight step
+    const label = chalk.yellowBright(step);
+
+    // Simple message (if env variable `NODE_DEBUG=${pluginName}` is set)
+    debugLogger(label, msg);
+
+    // Extended message (if env variable `NODE_DEBUG=${pluginName}-extended` is set)
+    // TODO: Find a way of not logging twice
+    if (extendedMsg) {
+      debugLoggerExtended(label, msg, `\n${extendedMsg}`);
+    }
+  };
+
+  /**
+   * Error logger
+   * @param {string} step - Title of current task step, will be highlighted
+   * @param {Error} err
+   * @param {boolean} [dev] - Whether we are in dev mode. Process exits otherwise.
+   */
+  const error = (err, dev) => {
+    log(chalk.blue(pluginName), err.fileName ? chalk.yellow(err.fileName) : '', chalk.red(err.message));
+
+    if (err.stack) {
+      debugLogger(chalk.red(pluginName), err.stack);
+    }
+
+    if (!dev) {
+      process.exit(1);
+    }
+  };
+
 
   return {
-
-    /**
-     * Simple info logger
-     * @param {*} msg - Log content
-     */
-    info: (msg) => {
-      log(chalk.blue(pluginName), msg);
-    },
-
-    /**
-     * Debug logger
-     * @param {string} step - Title of current task step, will be highlighted
-     * @param {*} msg - Log content
-     * @param {*} [extendedMsg] - Extended log content, will only be logged
-     * if `NODE_DEBUG=${pluginName}-extended` is set.
-     * The current Node version (9.5.0 at the time of writing) seems to support wildcards
-     * for sections, so `NODE_DEBUG=${pluginName}*` should be fine at some point, too.
-     */
-    debug: (step, msg, extendedMsg) => {
-      // Highlight step
-      const label = chalk.yellowBright(step);
-
-      // Simple message (if env variable `NODE_DEBUG=${pluginName}` is set)
-      debug(label, msg);
-
-      // Extended message (if env variable `NODE_DEBUG=${pluginName}-extended` is set)
-      // TODO: Find a way of not logging twice
-      if (extendedMsg) {
-        debugExtended(label, msg, `\n${extendedMsg}`);
-      }
-    },
-
-    /**
-     * Error logger
-     * @param {string} step - Title of current task step, will be highlighted
-     * @param {Error} err
-     * @param {boolean} [dev] - Whether we are in dev mode. Process exits otherwise.
-     */
-    error: (err, dev) => {
-      log(chalk.blue(pluginName), err.fileName ? chalk.yellow(err.fileName) : '', chalk.red(err.message));
-
-      if (!dev) {
-        process.exit(1);
-      }
-    },
+    info,
+    debug,
+    error,
   };
 }
 
