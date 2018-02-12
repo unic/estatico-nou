fb7befcf88dce3105199233807f74e269c3c931f
Thomas Jaggi <thomas.jaggi@unic.com>
2018-02-12 19:13:28 +0100

    estatico-imageversions: Improve test config

--- packages/estatico-imageversions/test/fixtures/default/imageversions.config.js
+++ packages/estatico-imageversions/test/fixtures/default/imageversions.config.js
@@ -3,14 +3,14 @@ module.exports = {
     '100x50',
     {
       size: '100x50',
-      focusPoint: '400,400',
+      focusPoint: '750,750',
     },
     {
       size: '600x300',
     },
     {
       size: '600x300',
-      focusPoint: '0,400',
+      focusPoint: '0,750',
     },
     {
       width: 300,
