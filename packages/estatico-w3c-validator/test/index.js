933510f8cab5ddd4ec75961154316fb615c0d90d
Ben Sonley <lbsonley@gmail.com>
2021-01-21 19:21:31 +0100

    fix(estatico-w3c-validator): make tests pass

--- packages/estatico-w3c-validator/test/index.js
+++ packages/estatico-w3c-validator/test/index.js
@@ -3,28 +3,35 @@ const sinon = require('sinon');
 const utils = require('@unic/estatico-utils').test;
 const path = require('path');
 const del = require('del');
+const { Logger } = require('@unic/estatico-utils');
 const task = require('../index.js');
 
 const defaults = {
   src: './test/fixtures/*.html',
   srcBase: './test/fixtures',
   dest: './test/results/',
+  logger: new Logger('estatico-w3c-validator'),
 };
 
-test.cb('default', (t) => {
-  const spy = sinon.spy(console, 'log');
+const sandbox = sinon.createSandbox();
+let spy;
 
+test.beforeEach(() => {
+  spy = sandbox.spy(defaults.logger, 'error');
+});
+
+test.afterEach(() => {
+  sandbox.restore();
+});
+
+test.afterEach.always(() => {
+  del(path.join(__dirname, '/results'));
+});
+
+test('default', t => new Promise((resolve) => {
   task(defaults, {
     dev: true,
   })().on('finish', () => {
-    spy.restore();
-
-    const log = utils.stripLogs(spy);
-
-    t.regex(log, /index\.html Linting error \(details above\)/);
-
-    t.end();
+    resolve(t.truthy(spy.calledOnce));
   });
-});
-
-test.afterEach(() => del(path.join(__dirname, '/results')));
+}));
