5860238254ddc7094e5f3bba623b8b570f22f3e2
Robin Löffel <robin.loeffel@unic.com>
2021-01-28 16:51:46 +0100

    chore: adapt eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,13 @@ module.exports = {
   extends: 'airbnb-base',
   ignorePatterns: [
     '**/dist',
-    '**/test/expected',
+    '**/test',
     '**/estatico-eslint/test/fixtures/main.js',
     '**/estatico-webpack/test/fixtures/async.js',
-    '**/estatico-boilerplate/src'
-  ]
+    '**/estatico-boilerplate/src',
+  ],
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+  },
 };
